d4d402c2ba28bd9658dcb9734d70d5184fe69a87
Nathan Graule <solarliner@gmail.com>
2019-11-28 15:39:58 +0100

    Linting/Formatting

--- hooks/url.ts
+++ hooks/url.ts
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import Axios from "../axios";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+
+import Axios from "../axios";
 
 export function useHTTP<T>(url: string, config?: AxiosRequestConfig) {
-	const [data, setData] = useState<AxiosResponse<T> | null>(null);
-	useEffect(() => {
-		Axios.get(url, config).then(setData);
-	}, []);
-	return data;
+  const [data, setData] = useState<AxiosResponse<T> | null>(null);
+  useEffect(() => {
+    Axios.get(url, config).then(setData);
+  }, []);
+  return data;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Toolbar, Button, Typography } from "@material-ui/core";
+import { Container, Toolbar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
